4cd628de81bd7a79153e80f5f3958dd5cbf7e832
KASCH <33941248+kasch17@users.noreply.github.com>
2023-10-28 17:58:12 +0200

    Final version added
    Added both the assignment and bonus solutions files.

--- assignment1_bonus.c
+++ assignment1_bonus.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include <limits.h>
 
-// Cost matrix elements
 struct score
 {
     int result;
@@ -12,12 +12,12 @@ struct score
     int side;
 };
 
-// Function to calculate minimum value between three integers
-int minimum(int diagonal, int upper, int side)
+// Function to calculate maximum value between three integers
+int maximum(int diagonal, int upper, int side)
 {
-    if (diagonal <= upper)
+    if (diagonal > upper)
     {
-        if (diagonal <= side)
+        if (diagonal > side)
         {
             return diagonal;
         }
@@ -28,7 +28,7 @@ int minimum(int diagonal, int upper, int side)
     }
     else
     {
-        if (upper <= side)
+        if (upper > side)
         {
             return upper;
         }
@@ -39,7 +39,45 @@ int minimum(int diagonal, int upper, int side)
     }
 }
 
-// Function to calculate minimum value between three integers, but only if selected as possible moves and non-negative
+int allowed_maximum(int diagonal_cost, int diagonal, int upper_cost, int upper, int side_cost, int side)
+{
+    if (diagonal == 0)
+    {
+        diagonal_cost = 0;
+    }
+    if (upper == 0)
+    {
+        upper_cost = 0;
+    }
+    if (side == 0)
+    {
+        side_cost = 0;
+    }
+
+    if (diagonal_cost > upper_cost)
+    {
+        if (diagonal_cost > side_cost)
+        {
+            return diagonal_cost;
+        }
+        else
+        {
+            return side_cost;
+        }
+    }
+    else
+    {
+        if (upper_cost > side_cost)
+        {
+            return upper_cost;
+        }
+        else
+        {
+            return side_cost;
+        }
+    }
+}
+
 int allowed_minimum(int diagonal_cost, int diagonal, int upper_cost, int upper, int side_cost, int side)
 {
     if ((diagonal == 0) || (diagonal_cost < 0))
@@ -78,10 +116,39 @@ int allowed_minimum(int diagonal_cost, int diagonal, int upper_cost, int upper, 
     }
 }
 
-int compute_alignment(char *X, int X_length, char *Y, int Y_length)
+// Choose random char from A, C, G, T
+char choose_random(char exclude)
+{
+    char characters[] = {'A', 'C', 'G', 'T'};
+
+    if (exclude != 'A' && exclude != 'C' && exclude != 'G' && exclude != 'T')
+    {
+        int random_index = rand() % 4;
+        return characters[random_index];
+    }
+    else
+    {
+        char choices[3];
+        int index = 0;
+        for (int i = 0; i < 4; i++)
+        {
+            if (characters[i] != exclude)
+            {
+                choices[index] = characters[i];
+                index++;
+            }
+        }
+
+        int random_index = rand() % 3;
+        return choices[random_index];
+    }
+}
+
+// Function to generate maximum cost strings
+int generate_maximum_cost_strings(char *X, int m, char *Y, int n)
 {
     // Cost matrix
-    struct score cost_matrix[X_length + 1][Y_length + 1];
+    struct score cost_matrix[m + 1][n + 1];
     int gap_cost;
     int mismatch_cost;
     printf("Insert the gap cost: ");
@@ -90,9 +157,9 @@ int compute_alignment(char *X, int X_length, char *Y, int Y_length)
     scanf("%d", &mismatch_cost);
 
     // Initialize Cost matrix
-    for (int i = 0; i < X_length + 1; i++)
+    for (int i = 0; i < m + 1; i++)
     {
-        for (int j = 0; j < Y_length + 1; j++)
+        for (int j = 0; j < n + 1; j++)
         {
             if (i == 0)
             {
@@ -118,87 +185,65 @@ int compute_alignment(char *X, int X_length, char *Y, int Y_length)
         }
     }
 
-    // Calculate Cost matrix
+    // Calculate Maximum Cost matrix
     int diagonal_cost = 0;
     int upper_cost = 0;
     int side_cost = 0;
-    int is_minimum = 0;
-    for (int i = 1; i < X_length + 1; i++)
+    int is_maximum = 0;
+    for (int i = 1; i < m + 1; i++)
     {
-        for (int j = 1; j < Y_length + 1; j++)
+        for (int j = 1; j < n + 1; j++)
         {
-            if (X[i - 1] != Y[j - 1])
-            {
-                diagonal_cost = cost_matrix[i - 1][j - 1].result + (mismatch_cost * 2);
-            }
-            else
-            {
-                diagonal_cost = cost_matrix[i - 1][j - 1].result;
-            }
+            diagonal_cost = cost_matrix[i - 1][j - 1].result + (mismatch_cost * 2);
             upper_cost = cost_matrix[i - 1][j].result + gap_cost;
             side_cost = cost_matrix[i][j - 1].result + gap_cost;
 
-            is_minimum = minimum(diagonal_cost, upper_cost, side_cost);
-            cost_matrix[i][j].result = is_minimum;
-            if (is_minimum == diagonal_cost)
+            is_maximum = maximum(diagonal_cost, upper_cost, side_cost);
+            cost_matrix[i][j].result = is_maximum;
+            if (is_maximum == diagonal_cost)
             {
                 cost_matrix[i][j].diagonal = 1;
             }
-            if (is_minimum == upper_cost)
+            if (is_maximum == upper_cost)
             {
                 cost_matrix[i][j].upper = 1;
             }
-            if (is_minimum == side_cost)
+            if (is_maximum == side_cost)
             {
                 cost_matrix[i][j].side = 1;
             }
         }
     }
 
-    // Print Cost matrix
-    printf("+\t_\t");
-    for (int i = 0; i < Y_length; i++)
+    // Print Maximum Cost matrix
+    for (int i = 0; i < m + 1; i++)
     {
-        printf("%c\t", Y[i]);
-        if (i == Y_length - 1)
-        {
-            printf("\n");
-        }
-    }
-    for (int i = 0; i < X_length + 1; i++)
-    {
-        if (i == 0)
-        {
-            printf("_\t");
-        }
-        else
-        {
-            printf("%c\t", X[i - 1]);
-        }
-        for (int j = 0; j < Y_length + 1; j++)
+        for (int j = 0; j < n + 1; j++)
         {
             printf("%d\t", cost_matrix[i][j].result);
-            if (j == Y_length)
+            if (j == n)
             {
                 printf("\n");
             }
         }
     }
 
-    // Calculate minimum cost
-    int minimum_cost = 0;
-    int x_position = X_length;
-    int y_position = Y_length;
-    int partial_cost = cost_matrix[X_length][Y_length].result;
+    // Calculate maximum cost
+    int maximum_cost = 0;
+    int x_position = m;
+    int y_position = n;
+    int partial_cost = cost_matrix[m][n].result;
     int move_cost = 0;
     int diagonal = 0;
     int upper = 0;
     int side = 0;
-    char aligned_X[100] = {'\0'};
-    char aligned_Y[100] = {'\0'};
+    char ACGT1;
+    char ACGT2;
+    char reversed_X[100] = {'\0'};
+    char reversed_Y[100] = {'\0'};
     int position = 0;
 
-    while ((partial_cost != 0))
+    while (partial_cost != 0)
     {
         diagonal_cost = cost_matrix[x_position - 1][y_position - 1].result;
         upper_cost = cost_matrix[x_position - 1][y_position].result;
@@ -214,77 +259,73 @@ int compute_alignment(char *X, int X_length, char *Y, int Y_length)
             if ((partial_cost - move_cost) == (mismatch_cost * 2))
             {
                 // Mismatch
-                aligned_X[position] = '*';
-                aligned_Y[position] = '*';
+                ACGT1 = choose_random('0');
+                ACGT2 = choose_random(ACGT1);
+                reversed_X[position] = ACGT1;
+                reversed_Y[position] = ACGT2;
             }
             else
             {
                 // Match
-                aligned_X[position] = X[x_position];
-                aligned_Y[position] = Y[y_position];
+                ACGT1 = choose_random('0');
+                reversed_X[position] = ACGT1;
+                reversed_Y[position] = ACGT1;
             }
         }
         else if ((move_cost == upper_cost) && (upper == 1))
         {
             // Gap in Y
             x_position--;
-            aligned_X[position] = X[x_position];
-            aligned_Y[position] = '_';
+            reversed_X[position] = choose_random('0');
+            reversed_Y[position] = '_';
         }
         else
         {
             // Gap in X
             y_position--;
-            aligned_X[position] = '_';
-            aligned_Y[position] = Y[y_position];
+            reversed_X[position] = '_';
+            reversed_Y[position] = choose_random('0');
         }
-        minimum_cost += (partial_cost - move_cost);
+        maximum_cost += (partial_cost - move_cost);
         partial_cost = move_cost;
-        position++;
-    }
-    while ((x_position) && (y_position) && (partial_cost == 0))
-    {
-        x_position--;
-        y_position--;
-        aligned_X[position] = X[x_position];
-        aligned_Y[position] = Y[y_position];
         position++;
     }
 
     // Reverse aligned strings
-    int len_X = strlen(aligned_X);
-    int len_Y = strlen(aligned_Y);
+    int len_X = strlen(reversed_X);
+    int len_Y = strlen(reversed_Y);
     for (int i = 0; i < len_X / 2; i++)
     {
-        char temp = aligned_X[i];
-        aligned_X[i] = aligned_X[len_X - 1 - i];
-        aligned_X[len_X - 1 - i] = temp;
+        char temp = reversed_X[i];
+        reversed_X[i] = reversed_X[len_X - 1 - i];
+        reversed_X[len_X - 1 - i] = temp;
     }
     for (int i = 0; i < len_Y / 2; i++)
     {
-        char temp = aligned_Y[i];
-        aligned_Y[i] = aligned_Y[len_Y - 1 - i];
-        aligned_Y[len_Y - 1 - i] = temp;
+        char temp = reversed_Y[i];
+        reversed_Y[i] = reversed_Y[len_Y - 1 - i];
+        reversed_Y[len_Y - 1 - i] = temp;
     }
 
-    printf("The aligned strings are:\nX -->\t%s\nY -->\t%s\n", aligned_X, aligned_Y);
+    printf("The maximum cost strings are:\nX -->\t%s\nY -->\t%s\n", reversed_X, reversed_Y);
 
-    return minimum_cost;
+    return maximum_cost;
 }
 
 int main()
 {
-    char X[] = "GATTACA";
-    char Y[] = "GTCGACGCA";
-    printf("Assignment 1\nThe predefined strings are:\nX -->\t%s\nY -->\t%s\nTo change them, modify the code.\n", X, Y);
-    int minimum_cost = 0;
-    if (strlen(X) < strlen(Y))
+    int m = 10;
+    int n = 10;
+    char X[m];
+    char Y[n];
+    int result;
+    printf("Assignment 1 - Bonus\nThe predefined values are:\nm -->\t%d\nn -->\t%d\nTo change them, modify the code.\n", m, n);
+    if (m <= n)
     {
-        minimum_cost = compute_alignment(X, strlen(X), Y, strlen(Y));
+        result = generate_maximum_cost_strings(X, m, Y, n);
     }
     else
     {
-        minimum_cost = compute_alignment(Y, strlen(Y), X, strlen(X));
+        result = generate_maximum_cost_strings(Y, n, X, m);
     }
-    printf("Cost: %d\n", minimum_cost);
 }
